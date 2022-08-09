219df64557ae5db1b25246a5926603b775ddb095
tysonlovett <92412154+tysonlovett@users.noreply.github.com>
2022-08-09 15:37:50 +1000

    Adding threading and attempting syncronisation

--- test/BoltTest.py
+++ test/BoltTest.py
@@ -6,16 +6,33 @@ import threading
 from spherov2 import scanner
 from spherov2.sphero_edu import EventType, SpheroEduAPI
 from spherov2.types import Color
-robots = ["SB-CBFC", "SB-A753"]
+robots = ["SB-CBFC", "SB-E987", "SB-B11E", "SB-41E0"]
+x = 0
+y = 0
+initiator = False
+
 
 def thread_function(name):
-    time.sleep(5)
     print("Testing Starting...")
     print("Connecting to Bolt...")
     toy = scanner.find_BOLT(toy_name=name)
     if toy is not None:
         print("Connected.")
         with SpheroEduAPI(toy) as droid:
+            global y
+            global initiator
+            y += 1
+            print(y)
+            if y == x:
+                initiator = True
+                print("Initiator true")
+            while not ((y == x) and (initiator == True)):
+                droid.set_main_led(Color(r=0, g=255, b=0)) #Sets whole set_matrix
+                print("Setting matrix")
+                if initiator == True:
+                    print("break")
+                    break
+            print(y)
             print("Testing Start...")
             droid.set_main_led(Color(r=0, g=255, b=0)) #Sets whole Matrix
             droid.reset_aim()
@@ -44,8 +61,10 @@ def thread_function(name):
             print("Testing End...")
 
 for robot in robots:
+    x += 1
     robot = threading.Thread(target=thread_function, args=(robot,))
     robot.start()
+    time.sleep(10)
 
 
   #droid.register_event(EventType.on_sensor_streaming_data, droid.SensorStreamingInfo) #how you would register to data (function name is custom)
