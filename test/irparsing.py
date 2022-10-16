613c7b8336e6e319ba4448594cce74886248c2f4
tysonlovett <92412154+tysonlovett@users.noreply.github.com>
2022-10-16 16:43:17 +1100

    Update irparsing.py

--- test/irparsing.py
+++ test/irparsing.py
@@ -8,16 +8,7 @@ from spherov2.sphero_edu import EventType, SpheroEduAPI
 from spherov2.types import Color
 robots = ["SB-8427", "SB-B85A"]
 
-message_channels = (4, )
 
-def on_ir_message_4(api, channel):
-    if channel != 4:
-        return
-    api.set_main_led(Color(255, 0, 0))
-    api.listen_for_ir_message(message_channels)
-
-    api.register_event(EventType.on_ir_message, on_ir_message_4)
-    api.listen_for_ir_message(message_channels)
 
 def thread_function(name):
     print("Testing Starting...")
@@ -26,19 +17,15 @@ def thread_function(name):
     if toy is not None:
         print("Connected.")
         with SpheroEduAPI(toy) as droid:
+
             index=robots.index(name)
             print("Testing Start...",index)
             if index == 0:
                 droid.set_main_led(Color(r=255, g=0, b=0)) #Sets whole set_matrix
-                droid.start_ir_broadcast(0,7)
-                time.sleep(5)
-                droid.send_ir_message(4,5)
-                    # droid.send_ir_message(0,30)
+                droid.listen_for_ir_message(4)
             if index == 1:
                 droid.set_main_led(Color(r=0, g=255, b=0)) #Sets whole set_matrix
-                droid.start_ir_follow(0,7)
-                droid.listen_for_ir_message(4, )
-                    # droid.listen_for_ir_message(0, 100)
+                droid.send_ir_message(4,5)
             time.sleep(10)
             print(droid.get_last_ir_message())
             print("Testing End...")
