56c438377284e63b91ce69152e1b18225062c363
tysonlovett <92412154+tysonlovett@users.noreply.github.com>
2022-10-19 11:31:37 +1100

    Update irfollow.py

--- test/irfollow.py
+++ test/irfollow.py
@@ -6,7 +6,7 @@ import threading
 from spherov2 import scanner
 from spherov2.sphero_edu import EventType, SpheroEduAPI
 from spherov2.types import Color
-robots = ["SB-8427", "SB-B85A"]
+robots = ["SB-8427", "SB-B85A", "SB-E987"]
 
 def thread_function(name):
     print("Testing Starting...")
@@ -20,11 +20,14 @@ def thread_function(name):
             print("Testing Start...",index)
             if index == 0:
                 droid.set_main_led(Color(r=255, g=0, b=0))
-                droid.start_ir_broadcast(0,7)
+                droid.start_ir_broadcast(0,1)
             if index == 1:
                 droid.set_main_led(Color(r=0, g=255, b=0))
-                droid.start_ir_follow(0,7)
-            time.sleep(10)
+                droid.start_ir_follow(0,1)
+            if index == 2:
+                droid.set_main_led(Color(r=0, g=255, b=0))
+                droid.start_ir_follow(0,1)
+            time.sleep(20)
             print(droid.get_last_ir_message())
             print("Testing End...")
 
